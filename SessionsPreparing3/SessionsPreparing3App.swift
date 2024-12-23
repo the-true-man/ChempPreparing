854e09d6d0f11cc6d79cb4e58ddc970880377a99
evgeny <wxnnvdie@gmail.com>
2024-12-23 13:15:34 +0500

    Initial Commit

---
+++ SessionsPreparing3/SessionsPreparing3App.swift
@@ -0,0 +1,17 @@
+//
+//  SessionsPreparing3App.swift
+//  SessionsPreparing3
+//
+//  Created by Евгений Михайлов on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SessionsPreparing3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
